deeeececc90641f72c2ae79c1e61626e5eea88bf
zhangw <935279748@qq.com>
2020-05-07 15:14:37 +0800

    update

--- src/pressScale/Item.tsx
+++ src/pressScale/Item.tsx
@@ -1,21 +1,29 @@
 import React, { useRef, useState, useEffect } from 'react';
 import Reparent from '../Reparent';
 
-const Item = ({
-  src,
-  container,
-  onActiveChange,
-}: {
+type Props = {
   src: string;
   container?: HTMLDivElement | null;
   onActiveChange: (active: boolean) => void;
-}) => {
+};
+
+const imgSty: React.CSSProperties = {
+  width: '45vw',
+  height: (75 * 45) / 50 + 'vw',
+};
+
+const containerSty: React.CSSProperties = {
+  width: '45vw',
+  height: 300,
+  backgroundColor: 'white',
+  margin: '2.5vw',
+  position: 'relative',
+};
+
+const Item = ({ src, container, onActiveChange }: Props) => {
   const [ref, setRef] = useState<HTMLDivElement | null>(null);
   const [con, setCon] = useState<HTMLDivElement | undefined | null>(container);
-  const [imgStyle, setImgStyle] = useState<React.CSSProperties>({
-    width: '45vw',
-    height: (75 * 45) / 50 + 'vw',
-  });
+  const [imgStyle, setImgStyle] = useState<React.CSSProperties>(imgSty);
   const imgRef = useRef<HTMLImageElement | null>(null);
   const dom = con || ref;
 
@@ -45,7 +53,6 @@ const Item = ({
         const scale = toWidth / width;
         const transform = `translate(-${left}px, -${top}px) scale(${scale})`;
         setImgStyle({ ...newImgStyle, transform });
-        console.log('timer end');
       }, 0);
     } else {
       setCon(container);
@@ -65,26 +72,12 @@ const Item = ({
   };
 
   return (
-    <div>
-      <div
-        style={{
-          width: '45vw',
-          height: 300,
-          backgroundColor: 'white',
-          margin: '2.5vw',
-          position: 'relative',
-        }}
-        ref={(dom) => {
-          setRef(dom);
-        }}
-        onClick={onClickFn}
-      >
-        <Reparent container={dom}>
-          <img ref={imgRef} src={src} style={imgStyle} alt='' />
-        </Reparent>
-        <div style={{ position: 'absolute', bottom: 0, left: 0 }}>
-          hello world
-        </div>
+    <div style={containerSty} ref={setRef} onClick={onClickFn}>
+      <Reparent container={dom}>
+        <img ref={imgRef} src={src} style={imgStyle} alt='' />
+      </Reparent>
+      <div style={{ position: 'absolute', bottom: 0, left: 0 }}>
+        hello world
       </div>
     </div>
   );
--- src/pressScale/index.tsx
+++ src/pressScale/index.tsx
@@ -1,32 +1,31 @@
 import React, { useState, useRef } from 'react';
 import Item from './Item';
 
-const App = () => {
-  const datas = [
-    {
-      src:
-        'https://static.ccrgt.com/images/0cdbc8fe-7f10-4486-9f07-740d93c76803.jpg',
-    },
-    {
-      src:
-        'https://static.ccrgt.com/images/e56f498c-66ca-4c74-82f4-27a6480a6258.png',
-    },
-  ];
+const datas = [
+  {
+    src:
+      'https://static.ccrgt.com/images/0cdbc8fe-7f10-4486-9f07-740d93c76803.jpg',
+  },
+  {
+    src:
+      'https://static.ccrgt.com/images/e56f498c-66ca-4c74-82f4-27a6480a6258.png',
+  },
+];
 
+const containerSty: React.CSSProperties = {
+  display: 'flex',
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+  width: '100vw',
+  height: '100vh',
+  position: 'relative',
+};
+
+const App = () => {
   const [active, setActive] = useState(-1);
   const ref = useRef<HTMLImageElement | null>(null);
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        width: '100vw',
-        height: '100vh',
-        position: 'relative',
-      }}
-      ref={ref}
-    >
+    <div style={containerSty} ref={ref}>
       {datas.map((data, index) => {
         return (
           <Item
